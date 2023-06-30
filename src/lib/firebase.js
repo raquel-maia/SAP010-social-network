b807d2bcec4fd5d2ca0e88ff9d7b9808d33e367f
Raquel Maia <70439400+raquel-maia@users.noreply.github.com>
2023-06-30 17:44:07 -0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore/lite';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8mmvKCsYEEcgP4TTJJnju5RopeT-PU2s',
-  authDomain: 'techsisters-a19d0.firebaseapp.com',
-  projectId: 'techsisters-a19d0',
-  storageBucket: 'techsisters-a19d0.appspot.com',
-  messagingSenderId: '713678266801',
-  appId: '1:713678266801:web:b9b1ee63bb5bf9d14b5d90',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 export const app = initializeApp(firebaseConfig);
